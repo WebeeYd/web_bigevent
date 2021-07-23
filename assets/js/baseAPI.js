564fd5d62f281585dafd44f622223280f71b3dee
yipan <782239846@qq.com>
2021-07-23 19:49:29 +0800

    index has been finished

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,24 @@
 $.ajaxPrefilter(function (option) {
     // console.log(option.url)
-    option.url="http://api-breakingnews-web.itheima.net"+option.url
+    // 在发起Ajax请求之前统一的设置以恶搞路径
+    option.url = "http://api-breakingnews-web.itheima.net" + option.url
     // console.log(option.url)
+    // 在发起Ajax请求之前统一的设置一个路径
+
+    // 为有权限的接口添加
+    if (option.url.indexOf("/my/") !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem("token") || ""
+        }
+    }
+    // 挂在完成函数
+    option.complete = function (data) {
+        console.log(data.responseJSON)
+        if (data.responseJSON.status !== 0) {
+            localStorage.removeItem("token")
+
+            location.href = "./login.html"
+        }
+    }
+
 })
---
+++ assets/js/index.js
@@ -0,0 +1,60 @@
+$(function(){
+    getUserinfo()
+    var layer = layui.layer
+    // 点击按钮进行退出的操作
+    $("#btnLoginout").click(function(){
+        // 点击按钮之后显示确认的弹框
+        layer.confirm('请问您是否确认退出?', {icon: 3, title:'提示'}, function(index){
+            //do something
+            // 点击退出确认 首先清除token
+            localStorage.removeItem('token')
+            // 然后调转页面
+            location.href = './login.html'
+            layer.close(index);
+          });
+    })
+})
+
+// 获取用户基本信息
+function getUserinfo(){
+    // 发送获取用户信息请求
+    layui.layer.msg("正在加载...",{
+        icon:3,
+        time:1000
+    })
+    $.ajax({
+        type:"get",
+        url:"/my/userinfo",
+        // 设置请求头
+        // headers:{
+        //     Authorization:localStorage.getItem("token")||""
+        // },
+        success:function(data){
+            if(data.status!=0){
+                return layui.layer.msg(data.message)
+            }
+            renderAvatar(data.data)
+            layui.layer.msg("加载成功！",{
+                icon:1,
+                time:1000
+            })
+        }
+    })
+}
+function renderAvatar(user){
+    // 显示用户名
+    var name = user.nickname || user.username
+    // 获取相应元素修改内容
+    $("#welcome").html("欢迎&nbsp;&nbsp;"+name)
+    // 按照需要来渲染头像
+    // 渲染图片头像
+    if(user.user_pic!=null){
+        $(".layui-nav-img").attr("src",user.user_pic).show()
+        $(".text-avatar").hide()
+    }
+    // 渲染文本头像
+    else{
+        $(".layui-nav-img").hide()
+        $(".text-avatar").html(name[0].toUpperCase()).show()
+    }
+}
